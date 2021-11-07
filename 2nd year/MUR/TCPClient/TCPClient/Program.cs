c835f85c4a978429c02851925da1456b5f3dbeb3
VehoTim <tim.vehovar@student.um.si>
2021-11-07 11:36:59 +0100

    TCP Client delujoca verzija s komentarji

---
+++ 2nd year/MUR/TCPClient/TCPClient/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Vaja02
+{
+    class TCPClient
+    {
+        public static string Decrypt(string encodedText, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.ASCII.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Convert.FromBase64String(encodedText);
+
+            string plaintext = Encoding.ASCII.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return plaintext;
+        }
+
+        private static void Main(String[] args)
+        {
+            byte[] prejeto = new byte[1024];                                                //sem se bo shranilo sporocilo serverja
+            
+            IPAddress ipNaslov = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];        
+            IPEndPoint ipCilj = new IPEndPoint(ipNaslov, 1234);                             //cilj je ip naslov in vrata preko katerih se bomo povezali (lahko so karkoli)
+
+            string vnos;                    //uporabnikov vnos (glava in sporocilo)
+            string paket = "";              //paket bo sestavljen iz glave in sporocila
+            bool nadaljuj;                  
+
+            Socket client = new Socket(ipNaslov.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            
+            try
+            {
+                client.Connect(ipCilj);             //posljemo prosnjo za povezavo s serverjem
+
+                //Console.WriteLine("Vtičnica narejena do " + client.RemoteEndPoint.ToString());
+
+                do
+                {
+                    Console.Write("Vpisi crko (ce bo vnesenih vec crk se bo gledala samo prva): ");
+                    vnos = Console.ReadLine();
+                    nadaljuj = false;
+
+                    switch (vnos.ToString().ToUpper()[0])
+                    {
+                        case 'A':
+                            paket = vnos;
+                            break;
+                        case 'B':
+                            paket = vnos;
+                            break;
+                        case 'C':
+                            paket = vnos;
+                            break;
+                        case 'D':
+                            paket = vnos;
+                            Console.WriteLine("Vpisi sporocilo: ");
+                            vnos = Console.ReadLine();
+                            paket += vnos;
+                            break;
+                        case 'E':
+                            paket = vnos;
+                            break;
+                        case 'F':
+                            paket = vnos;
+                            Console.WriteLine("Vpisi Forsyth-Edwards notacijo: ");
+                            vnos = Console.ReadLine();
+                            paket += vnos;
+                            break;
+                        case 'G':
+                            paket = vnos;
+                            Console.WriteLine("Vpisi sporocilo, ki bo kodirano: ");
+                            vnos = Console.ReadLine();
+                            paket += vnos;
+                            break;
+                        default:
+                            nadaljuj = true;
+                            Console.WriteLine("Vnesel si napacen tip sporocila. Poskusi enega izmed: A/B/C/D/E/F/G");
+                            break;
+                    }
+                } while (nadaljuj);
+                
+                byte[] sporocilo = Encoding.ASCII.GetBytes(paket);      //paket zapisemo v sporocilo iz bytov
+
+                int n = client.Send(sporocilo);                         //paket posljemo
+
+                int m = client.Receive(prejeto);                        //nazaj prejmemo sporocilo od serverja
+
+                if (paket.ToString().ToUpper()[0] == 'G')               //ce je glava sporocila enaka G, je potrebno se nekaj modifikacij in dekodiranje 
+                {
+                    string x = Encoding.Default.GetString(prejeto);
+                    x = x.Replace("\0", string.Empty);
+
+                    Console.WriteLine(x);
+                    Console.WriteLine(Decrypt(x, "mykey"));
+                }
+                else Console.WriteLine(Encoding.Default.GetString(prejeto));
+
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+
+            }
+            catch           //ce se povezava ne izzide izpisemo to sporocilo
+            {
+                Console.WriteLine("Povezava je neuspesna. Server verjetno ne operatuje.");
+            }
+
+            Console.WriteLine("Konec prenašanja.");
+        }
+    }
+}
---
+++ 2nd year/MUR/TCPServer/TCPServer/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Vaja02
+{
+    class TCPServer
+    {
+        static public string FEN(string s)
+        {
+            string odgovor = "";
+            int v = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (v == 8) odgovor += '\n';
+                if (Char.IsLetter(s[i]))
+                {
+                    odgovor += s[i];
+                    v++;
+                }
+                else if (Char.IsDigit(s[i]))
+                {
+                    for (int j = 0; j < int.Parse(s[i].ToString()); j++)
+                    {
+                        //s[i];
+                        odgovor += ' ';
+                        v++;
+                    }
+                }
+                else if (s[i] == '/')
+                {
+                    v = 0;
+                    continue;
+                }
+                else if (s[i] == ' ')
+                {
+                    if (s[i + 1] == 'w') odgovor += "\n\nNa vrsti beli";
+                    else odgovor += "\n\nNa vrsti crni";
+
+                    odgovor += "\n\nMoznosti rokade:";
+                    int j = 3;
+                    while (s[i + j] != ' ' && s[i + j] != '-')
+                    {
+                        if (s[i + j] == 'K')
+                            odgovor += "\nBeli kraljeva stran";
+                        if (s[i + j] == 'Q')
+                            odgovor += "\nBeli damina stran";
+                        if (s[i + j] == 'k')
+                            odgovor += "\nCrni kraljeva stran";
+                        if (s[i + j] == 'q')
+                            odgovor += "\nCrni damina stran";
+                        j++;
+                    }
+                    j++;
+
+                    odgovor += "\n\nMoznosti en passant:";
+                    if (s[i + j] == '-')
+                    {
+                        odgovor += "ne";
+                        j++;
+                    }
+                    else
+                    {
+                        odgovor += s[i + j].ToString() + s[i + j + 1].ToString();
+                        j += 2;
+                    }
+                    j++;
+
+                    odgovor += "\n\nStevilo polpotez: " + s[i + j];
+                    j += 2;
+                    odgovor += "\n\nStevilka trenutne poteze: " + s[i + j];
+                    break;
+                }
+            }
+            
+            return odgovor;
+        }
+
+        static public string Encrypt(string source, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.ASCII.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Encoding.ASCII.GetBytes(source);
+
+            string encoded =
+                Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return encoded;
+        }
+
+        static void Main(string[] args)
+        {
+            byte[] buffer = new byte[1024];                             //en byte za glavo prejetega paketa ostalo za sporocilo (maksimalna dolzina sporocila v tem primeru je 1023)
+            string data = null;                                         //sem se bo shranila vsebina prejetega sporocila
+            char header;                                                //sem se bo shranila glava prejetega sporocila
+
+            byte[] msg = Encoding.ASCII.GetBytes("");                   //to bo poslano clientu
+
+
+            IPAddress ipNaslov = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];    
+            IPEndPoint cilj = new IPEndPoint(ipNaslov, 1234);                           //cilj je ip naslov in vrata preko katerih se bomo povezali (lahko so karkoli)
+
+            Socket vticnica = new Socket(ipNaslov.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            vticnica.Bind(cilj);                                        //povezemo vticnico z ciljem
+            vticnica.Listen(100);
+
+            while (true)
+            {
+
+                Console.WriteLine("\nPoslušam na naslovu " + vticnica.LocalEndPoint.ToString());
+                Socket povezava = vticnica.Accept();                                                        //sprejmemo prosnjo za povezavo z clientom, ko ta pride
+                Console.WriteLine("\nOdjemalec se je povezal na " + povezava.LocalEndPoint.ToString());
+
+                int b = povezava.Receive(buffer);                       //dolzina paketa
+                data += Encoding.ASCII.GetString(buffer, 0, b);         //paket shranimo v string
+
+                header = data.ToString().ToUpper()[0];                  //ter ga locimo na glavo in sporocilo
+                Console.WriteLine("Glava sporocila: " + header);
+                data = data.Remove(0, 1);
+                Console.WriteLine("Vsebina sporocila: " + data);
+
+                switch (header)
+                {
+                    case 'A':
+                        msg = Encoding.ASCII.GetBytes("Pozdravljen " + povezava.LocalEndPoint.ToString());
+                        break;
+                    case 'B':
+                        msg = Encoding.ASCII.GetBytes("Trenutni cas je " + DateTime.Now);
+                        break;
+                    case 'C':
+                        msg = Encoding.ASCII.GetBytes("Trenuten delovni direktorij je " + Directory.GetCurrentDirectory());
+                        break;
+                    case 'D':
+                        msg = Encoding.ASCII.GetBytes(data.ToString());
+                        break;
+                    case 'E':
+                        msg = Encoding.ASCII.GetBytes("Ime naprave: " + Environment.MachineName + ", verzija os: " + Environment.OSVersion);
+                        break;
+                    case 'F':
+                        msg = Encoding.ASCII.GetBytes(FEN(data));
+                        break;
+                    case 'G':
+                        string x = Encrypt(data, "mykey");
+                        msg = Encoding.ASCII.GetBytes(x);
+                        break;
+                    default:
+                        msg = Encoding.ASCII.GetBytes("Vnesel si napačen tip sporočila. Poskusi enega izmed: A/B/C/D/E/F/G");
+                        break;
+                }
+                data = null;
+
+                Console.WriteLine("Odgovoril sem: " + Encoding.Default.GetString(msg));
+                povezava.Send(msg);
+                povezava.Close();
+            }
+        }
+    }
+}
