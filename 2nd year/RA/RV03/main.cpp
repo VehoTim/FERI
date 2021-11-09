72d1f4b4616909744d87cb7d15fe50b2b68d5774
VehoTim <tim.vehovar@student.um.si>
2021-11-09 18:19:39 +0100

    RV03 RA nadgradnja programa potrebno se malce ocistiti in dodati preverjanje v tocki 3?

--- 2nd year/RA/RV03/main.cpp
+++ 2nd year/RA/RV03/main.cpp
@@ -2,6 +2,34 @@
 
 using namespace std;
 
+void printMeni(){
+    cout << "Meni:" << endl;
+    cout << "1) Dekodiranje" << endl;
+    cout << "2) Kodiranje" << endl;
+    cout << "0) Izhod" << endl;
+    cout << endl << "Izbira: ";
+}
+
+bool preveriZnak(int i){
+    if(i > 1114111){
+        cout << "Znak ne obstaja" << endl;
+        return false;
+    }
+    else if(i == 32){
+        cout << "Vpisan znak 32 (space)" << endl;
+    }
+    else if(i == 127){
+        cout << "Vpisan znak 127 (delete)" << endl;
+    }
+    else cout << "Vpisan znak: " << char(i) << endl;
+    return true;
+}
+
+bool preveriZnak2(string s){
+    if (s.length() == 2 && toupper(s[0]) =='F' && (toupper(s[1]) =='F' || toupper(s[1]) =='E')) return false;
+    return true;
+}
+
 int pow(int x, int p)
 {
     if (p == 0) return 1;
@@ -17,6 +45,8 @@ void printKodiranje(int znak){
     bool byte2[8];
     bool byte3[8];
     bool byte4[8];
+    byte2[0] = byte3[0] = byte4[0] = 1;
+    byte2[1] = byte3[1] = byte4[1] = 0;
     if(znak < 128){
         byte1[0] = 0;
         for (int i = 7; i > 0; --i) {
@@ -32,8 +62,6 @@ void printKodiranje(int znak){
         byte1[0] = 1;
         byte1[1] = 1;
         byte1[2] = 0;
-        byte2[0] = 1;
-        byte2[1] = 0;
         for (int i = 7; i > 1; --i) {
             byte2[i] = znak % 2;
             znak = znak / 2;
@@ -56,10 +84,6 @@ void printKodiranje(int znak){
         byte1[1] = 1;
         byte1[2] = 1;
         byte1[3] = 0;
-        byte2[0] = 1;
-        byte2[1] = 0;
-        byte3[0] = 1;
-        byte3[1] = 0;
         for (int i = 7; i > 1; --i) {
             byte3[i] = znak % 2;
             znak = znak / 2;
@@ -91,12 +115,6 @@ void printKodiranje(int znak){
         byte1[2] = 1;
         byte1[3] = 1;
         byte1[4] = 0;
-        byte2[0] = 1;
-        byte2[1] = 0;
-        byte3[0] = 1;
-        byte3[1] = 0;
-        byte4[0] = 1;
-        byte4[1] = 0;
         for (int i = 7; i > 1; --i) {
             byte4[i] = znak % 2;
             znak = znak / 2;
@@ -129,8 +147,8 @@ void printKodiranje(int znak){
         for (int i = 0; i < 8; ++i) {
             cout << byte4[i];
         }
-
     }
+    cout << endl;
 }
 
 void printDekodiranje(string znak){
@@ -151,30 +169,41 @@ void printDekodiranje(string znak){
         }
     }
 
-    //po potrebi odstrani
-    cout << "Vnesen znak je " << char(vrednost) << "(" << vrednost << ")";
+    if (char(vrednost) == 32) cout << "Vnesen znak je presledek (32)" << endl;
+    else if (char(vrednost) == 127) cout << "Vnesen znak je delete (127)" << endl;
+    else if (vrednost > 1114111) cout << "Vnesen znak ne obstaja." << endl;
+    else cout << "Vnesen znak je " << vrednost  << endl;
 }
 
 int main() {
+    bool nadaljuj = true;
+    int izbira;
     int kodZnak = 0;
-    cout << "Vpisi kodZnak v obliki desetiskega stevila:";
-    cin >> kodZnak;
-    if(kodZnak > 1114111){
-        cout << "Znak ne obstaja";
-        return 1;
-    }
-    else if(kodZnak == 32){
-        cout << "Vpisan kodZnak 32 (space)" << endl;
-    }
-    else if(kodZnak == 127){
-        cout << "Vpisan kodZnak 127 (delete)" << endl;
-    }
-    else cout << "Vpisan kodZnak: " << char(kodZnak) << endl;
-    printKodiranje(kodZnak);
-
     string dekZnak;
-    cout << "Vpisi kodZnak v obliki heksadecimalnega stevila:";
-    getline(cin, dekZnak);
-    printDekodiranje(dekZnak);
+    do {
+        printMeni();
+        cin >> izbira;
+
+        switch (izbira) {
+            case 1:
+                cout << "Vpisi znak v obliki desetiskega stevila:";
+                cin >> kodZnak;
+                if (preveriZnak(kodZnak)) printKodiranje(kodZnak);
+                break;
+            case 2:
+                cout << "Vpisi znak v obliki heksadecimalnega stevila:";
+                cin.ignore();
+                getline(cin, dekZnak);
+                printDekodiranje(dekZnak);
+                break;
+            case 0:
+                nadaljuj = false;
+                break;
+            default:
+                cout << "Napacen vnos poskusi povnovno" << endl;
+                break;
+        }
+    }while(nadaljuj);
+
     return 0;
 }
