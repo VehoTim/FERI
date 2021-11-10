cc30980a8f38cb2b8791e5a401e1be1e421ce6d0
VehoTim <tim.vehovar@student.um.si>
2021-11-11 00:56:40 +0100

    RV03 RA program uspešno preverja obliko UTF8 in ne dekodira ce je oblika napacna

--- 2nd year/RA/RV03/main.cpp
+++ 2nd year/RA/RV03/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 void printMeni(){
     cout << "Meni:" << endl;
-    cout << "1) Dekodiranje" << endl;
-    cout << "2) Kodiranje" << endl;
+    cout << "1) Kodiranje" << endl;
+    cout << "2) Dekodiranje" << endl;
     cout << "0) Izhod" << endl;
     cout << endl << "Izbira: ";
 }
 
-bool preveriZnak(int i){
+bool preveriZnak(unsigned int i){
     if(i > 1114111){
         cout << "Znak ne obstaja" << endl;
         return false;
@@ -25,9 +25,76 @@ bool preveriZnak(int i){
     return true;
 }
 
-bool preveriZnak2(string s){
-    if (s.length() == 2 && toupper(s[0]) =='F' && (toupper(s[1]) =='F' || toupper(s[1]) =='E')) return false;
-    return true;
+bool preveriUTF(unsigned int utf){
+    bool byte1[8];
+    bool byte2[8];
+    bool byte3[8];
+    bool byte4[8];
+    unsigned int tmp = utf;
+
+    if(utf < 256 && utf > 0){
+        for (int i = 7; i >= 0; --i) {
+            byte1[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+
+        if (byte1[0] == 1) return false;
+        else return true;
+    }
+    else if(utf < 65535){
+        for (int i = 7; i >= 0; --i) {
+            byte2[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte1[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        if((byte1[0] == 0 || byte1[1] == 0 || byte1[2] == 1 || byte2[0] == 0 || byte2[1] == 1) || (utf - 49280) <= 0) return false;
+        else return true;
+    }
+    else if(utf < 16777215){
+        for (int i = 7; i >= 0; --i) {
+            byte3[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte2[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte1[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+
+        if((byte1[0] == 0 || byte1[1] == 0 || byte1[2] == 0 || byte1[3] == 1 || byte2[0] == 0 || byte2[1] == 1 ||
+           byte3[0] == 0 || byte3[1] == 1) || (utf - 14712960) <= 0) return false;
+        else return true;
+    }
+    else if(utf < 4294967295 ){
+        for (int i = 7; i >= 0; --i) {
+            byte4[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte3[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte2[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+        for (int i = 7; i >= 0; --i) {
+            byte1[i] = tmp % 2;
+            tmp = tmp / 2;
+        }
+
+        if((byte1[0] == 0 || byte1[1] == 0 || byte1[2] == 0 || byte1[3] == 0 || byte1[4] == 1 || byte2[0] == 0 ||
+           byte2[1] == 1 || byte3[0] == 0 || byte3[1] == 1 || byte4[0] == 0 || byte4[1] == 1) || (utf - 4034953344) <= 0)
+                return false;
+        else return true;
+    }
+    else return false;
 }
 
 int pow(int x, int p)
@@ -40,7 +107,7 @@ int pow(int x, int p)
     else return x * tmp * tmp;
 }
 
-void printKodiranje(int znak){
+void printKodiranje(unsigned int znak){
     bool byte1[8];
     bool byte2[8];
     bool byte3[8];
@@ -54,13 +121,10 @@ void printKodiranje(int znak){
             znak = znak / 2;
         }
         cout << "UTF-8 binarna oblika: ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte1[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte1[i];
     }
-    else if(znak < 2047){
-        byte1[0] = 1;
-        byte1[1] = 1;
+    else if(znak < 2048){
+        byte1[0] = byte1[1] = 1;
         byte1[2] = 0;
         for (int i = 7; i > 1; --i) {
             byte2[i] = znak % 2;
@@ -71,18 +135,12 @@ void printKodiranje(int znak){
             znak = znak / 2;
         }
         cout << "UTF-8 binarna oblika: ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte1[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte1[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte2[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte2[i];
     }
-    else if(znak < 65535){
-        byte1[0] = 1;
-        byte1[1] = 1;
-        byte1[2] = 1;
+    else if(znak < 65536){
+        byte1[0] = byte1[2] = byte1[1] = 1;
         byte1[3] = 0;
         for (int i = 7; i > 1; --i) {
             byte3[i] = znak % 2;
@@ -97,23 +155,14 @@ void printKodiranje(int znak){
             znak = znak / 2;
         }
         cout << "UTF-8 binarna oblika: ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte1[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte1[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte2[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte2[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte3[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte3[i];
     }
     else if(znak < 1114111 ){
-        byte1[0] = 1;
-        byte1[1] = 1;
-        byte1[2] = 1;
-        byte1[3] = 1;
+        byte1[0] = byte1[1] = byte1[2] = byte1[3] = 1;
         byte1[4] = 0;
         for (int i = 7; i > 1; --i) {
             byte4[i] = znak % 2;
@@ -132,47 +181,45 @@ void printKodiranje(int znak){
             znak = znak / 2;
         }
         cout << "UTF-8 binarna oblika: ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte1[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte1[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte2[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte2[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte3[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte3[i];
         cout << " ";
-        for (int i = 0; i < 8; ++i) {
-            cout << byte4[i];
-        }
+        for (int i = 0; i < 8; ++i) cout << byte4[i];
     }
     cout << endl;
 }
 
 void printDekodiranje(string znak){
-    int vrednost = 0;
-    int utez = 0;
-    for (int i = znak.length() - 1; i >= 0 ; --i) {
-        if (znak[i] < 58 && znak[i] != 32){
-            vrednost += (znak[i] - '0') * pow(16, utez);
-            utez++;
-        }
-        else if (znak[i] > 64 && znak[i] < 91){
-            vrednost += (znak[i] - 'A' + 10) * pow(16, utez);
-            utez++;
-        }
-        else if(znak[i] > 96 && znak[i]  < 123){
-            vrednost += (znak[i] - 'a' + 10) * pow(16, utez);
-            utez++;
-        }
-    }
+    if(!(znak.length() == 2 && toupper(znak[0]) =='F' && (toupper(znak[1]) =='F' || toupper(znak[1]) =='E'))){
 
-    if (char(vrednost) == 32) cout << "Vnesen znak je presledek (32)" << endl;
-    else if (char(vrednost) == 127) cout << "Vnesen znak je delete (127)" << endl;
-    else if (vrednost > 1114111) cout << "Vnesen znak ne obstaja." << endl;
-    else cout << "Vnesen znak je " << vrednost  << endl;
+        unsigned int vrednost = 0;
+        int utez = 0;
+        for (int i = znak.length() - 1; i >= 0 ; --i) {
+            if (znak[i] < 58 && znak[i] != 32){
+                vrednost += (znak[i] - '0') * pow(16, utez);
+                utez++;
+            }
+            else if (znak[i] > 64 && znak[i] < 91){
+                vrednost += (znak[i] - 'A' + 10) * pow(16, utez);
+                utez++;
+            }
+            else if(znak[i] > 96 && znak[i]  < 123){
+                vrednost += (znak[i] - 'a' + 10) * pow(16, utez);
+                utez++;
+            }
+        }
+
+        if(preveriUTF(vrednost)){
+            if (char(vrednost) == 32) cout << "Vnesen znak je presledek (32)" << endl;
+            else if (char(vrednost) == 127) cout << "Vnesen znak je delete (127)" << endl;
+            else cout << "Vnesen znak je " << char(vrednost) << "(" << vrednost << ")"  <<endl;
+        }
+        else cout << "Vnesen znak ne obstaja." << endl;
+    }
+    else cout << "Ta koda ni dovoljena za dekodiranje";
 }
 
 int main() {
