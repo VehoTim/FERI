8df3d479ffcfe9353dcdc3ef5a79b4b73aa6fbb3
Tim Vehovar <tim.vehovar@gmail.com>
2021-12-06 23:16:46 +0100

    Dodana izbris funkcijo

--- 2nd year/OS/04_procesi/imenik/src/main.rs
+++ 2nd year/OS/04_procesi/imenik/src/main.rs
@@ -3,6 +3,7 @@
 
 //imenik
 
+//funkcija za branje in dodajanje vnosa
 fn beri(stevilke: &mut Vec<String>, imena: &mut Vec<String>, priimki: &mut Vec<String>) -> bool {
 
     //dodajanje telefonske stevilke
@@ -13,34 +14,64 @@ fn beri(stevilke: &mut Vec<String>, imena: &mut Vec<String>, priimki: &mut Vec<S
     //preverimo ali je stevilka ze v vektorju (ce da koncamo funkcijo)
     for st in stevilke.iter(){
         if st.to_string() == stevilka {
-            return false
+            return false;
         }
     }
 
-    stevilke.push(stevilka);
+    if stevilka.chars().count() <= 30 {
+        stevilke.push(stevilka);
+    } else {
+        println!("Vnesenih je bilo prevec znakov");
+        return false;
+    }
+
     
     //dodajanje imena
     println!("Vpisi ime: ");
     let mut ime = String::new();
     std::io::stdin().read_line(&mut ime);
-    
-    imena.push(ime);
+
+    if ime.chars().count() <= 100 {
+        imena.push(ime);
+    } else {
+        println!("Vnesenih je bilo prevec znakov");
+        return false;
+    }
 
      //dodajanje priimka
      println!("Vpisi priimek: ");
      let mut priimek = String::new();
      std::io::stdin().read_line(&mut priimek);
-     
-     priimki.push(priimek);
+
+     if priimek.chars().count() <= 100 {
+        priimki.push(priimek);
+    } else {
+        println!("Vnesenih je bilo prevec znakov");
+        return false;
+    }
 
     return true
 }
 
+//funkcija za brisanje vnosov
+fn izbris(stevilka: String, stevilke: &mut Vec<String>, imena: &mut Vec<String>, priimki: &mut Vec<String>) -> bool {
+    //iskanje po vektorju za ujemanje
+    let mut n = 0;
+    while n < stevilke.len() {
+        if stevilka == stevilke[n].to_string() {
+            priimki.remove(n);
+            imena.remove(n);
+            stevilke.remove(n);
+            return true;
+        }
+        n += 1;
+    } 
+    return false;
+}
+
 fn main() {
     let mut imena: Vec<String> = Vec::new();
-
     let mut priimki: Vec<String> = Vec::new();
-
     let mut stevilke: Vec<String> = Vec::new();
 
     if beri(&mut stevilke, &mut imena, &mut priimki){
@@ -49,7 +80,15 @@ fn main() {
         println!("Vnos s to stevilko ze obstaja");
     }
 
-    for st in stevilke {
-        println!("Stevilka: {}", st);
+    println!("Vpisi stevilko: ");
+    let mut stevilka = String::new();
+    std::io::stdin().read_line(&mut stevilka);
+
+    if izbris(stevilka, &mut stevilke, &mut imena, &mut priimki) {
+        println!("Uspesno izbrisan");
+    } else {
+        println!("Vnos s to stevilko ne obstaja");
     }
+
+    //println!("{}", stevilke.len());
 }
