58c97e73b5778bab1885113f7c517429a42908f2
Tim Vehovar <tim.vehovar@gmail.com>
2022-01-17 10:53:17 +0100

    Removed duplicate files

--- 2nd year/OS/04_procesi/imenik/target/debug/build/libc-f9bfef8756e263e8/build_script_build-f9bfef8756e263e8.d
+++
@@ -1,5 +0,0 @@
-/home/vehotim/OS/imenik/target/debug/build/libc-f9bfef8756e263e8/build_script_build-f9bfef8756e263e8: /home/vehotim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs
-
-/home/vehotim/OS/imenik/target/debug/build/libc-f9bfef8756e263e8/build_script_build-f9bfef8756e263e8.d: /home/vehotim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs
-
-/home/vehotim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.109/build.rs:
--- 2nd year/OS/04_procesi/imenik_vpis/target/debug/deps/imenik_vpis-42419a5c8b35e050.d
+++
@@ -1,5 +0,0 @@
-/home/vehotim/OS/imenik_vpis/target/debug/deps/imenik_vpis-42419a5c8b35e050: src/main.rs
-
-/home/vehotim/OS/imenik_vpis/target/debug/deps/imenik_vpis-42419a5c8b35e050.d: src/main.rs
-
-src/main.rs:
