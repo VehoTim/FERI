58928a75100e3ebe3966ac86d353acc4b8c5231c
Tim Vehovar <tim.vehovar@gmail.com>
2022-03-09 20:06:55 +0100

    SiS (normalizacija dela, vendar ne gre skozi test)

---
+++ 2nd year/2_semester/SiS/osnove.py
@@ -0,0 +1,67 @@
+import numpy as np
+import random
+
+def generiraj_vektor_nakljucno(stevilo_vzorcev):
+    return np.random.uniform(0, 1, (stevilo_vzorcev, 1))
+
+def beri_zvok_mono(pot_do_datoteke):
+    from scipy.io import wavfile
+    Fs, y = wavfile.read(pot_do_datoteke)
+
+    if(y.ndim == 1):
+        vektor = y.reshape((-1, 1))
+        return vektor / np.max(np.abs(vektor))
+    else:
+        #zdruzi
+        vektor = np.mean(y, axis=1)
+        vektor = vektor.reshape((-1, 1))
+        return vektor / np.max(np.abs(vektor))
+
+def beri_zvok_stereo(pot_do_datoteke):
+    from scipy.io import wavfile
+    Fs, y = wavfile.read(pot_do_datoteke)
+
+    if(y.ndim == 1):
+        vektor = np.zeros((y.size,2))
+        for n in range(y.size):
+            vektor[n][0] = y[n]
+            vektor[n][1] = y[n]
+
+        return vektor / np.max(np.abs(vektor))
+    else: 
+        vektor = y
+        vektor = np.reshape(vektor,(-1,2))
+        return vektor / np.max(np.abs(vektor))
+
+def normaliziraj(vektor):
+    oblika = vektor.shape
+    tmp = np.reshape(vektor, (-1))
+    maxNorm = 0
+    for x in tmp:
+        if abs(x) > maxNorm:
+            maxNorm = abs(x)
+
+    i = 0
+    while i < tmp.size:
+        tmp[i] = tmp[i] / maxNorm
+        i += 1
+            
+    return np.reshape(tmp, oblika)
+
+def normaliziraj_vektorsko(vektor):
+    return vektor / np.max(np.abs(vektor))
+
+import timeit
+
+def testiraj_cas_izvajanja(vektor):
+    start = timeit.default_timer()
+    normaliziraj(vektor)
+    cas = timeit.default_timer() - start
+
+    start2 = timeit.default_timer()
+    normaliziraj_vektorsko(vektor)
+    cas2 = timeit.default_timer() - start2
+    return np.array([[cas, cas2]])
+
+if __name__ == '__main__':
+    print(normaliziraj(generiraj_vektor_nakljucno(3)))
