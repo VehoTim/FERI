1402c89434268f6d8b34ffbebd377176cab8e06a
Tim Vehovar <tim.vehovar@gmail.com>
2022-05-07 11:46:36 +0200

    UI - 03 Vecplastna nevronska mreza
    GUI za risanje in vektorizacija ter normalizacija znakov koncana

---
+++ 2nd year/2_semester/UI/Vaja03 - Večplastna nevronska mreža/Vaja03 - Večplastna nevronska mreža/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Vaja03___Večplastna_nevronska_mreža
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numVectors = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVectors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numVectors);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(276, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 406);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Initialize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(101, 149);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(69, 23);
+            this.numericUpDown4.TabIndex = 7;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(91, 105);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(59, 23);
+            this.numericUpDown3.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(166, 61);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(43, 23);
+            this.numericUpDown2.TabIndex = 5;
+            // 
+            // numVectors
+            // 
+            this.numVectors.Location = new System.Drawing.Point(121, 17);
+            this.numVectors.Name = "numVectors";
+            this.numVectors.Size = new System.Drawing.Size(58, 23);
+            this.numVectors.TabIndex = 4;
+            this.numVectors.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Error tollerance:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Learning rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of hidden neurons:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vectors per pattern:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(266, 406);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 417);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVectors)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numVectors;
+        private Button button1;
+        private Panel panel1;
+    }
+}
---
+++ 2nd year/2_semester/UI/Vaja03 - Večplastna nevronska mreža/Vaja03 - Večplastna nevronska mreža/Form1.cs
@@ -0,0 +1,228 @@
+namespace Vaja03___Večplastna_nevronska_mreža
+{
+    public partial class Form1 : Form
+    {
+        public struct Seznam
+        {
+            public string znak;
+            public List<Normalizirana> tocke;
+
+            public Seznam(string znak, List<Normalizirana> tocke)
+            {
+                this.znak = znak; this.tocke = new List<Normalizirana>(tocke);
+            }
+        }
+
+        public struct Normalizirana
+        {
+            public double x, y;
+
+            public Normalizirana(double x, double y) { this.x = x; this.y = y; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+
+        bool clicked = false;
+        Point previousPoint;
+        public Graphics g;
+
+        List<Point> points = new List<Point>();
+        List<Normalizirana> normalizirane = new List<Normalizirana>();
+        List<Seznam> vnosi = new List<Seznam> ();
+
+        bool ucenje = true;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ucenje)
+            {
+
+
+                ucenje = false;
+            }
+            else
+            {
+
+            }
+
+            //panel1.Invalidate();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel1.Invalidate();
+
+            clicked = true;
+            previousPoint = e.Location;
+
+            points.Clear();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clicked)
+            {
+                g.DrawLine(Pens.Black, previousPoint, e.Location);
+                points.Add(previousPoint);
+                points.Add(e.Location);
+                previousPoint = e.Location;
+
+                Invalidate();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (ucenje)
+            {
+
+
+                clicked = false;
+
+                string znak = PupUpForm.ShowDialog("Vpisi znacko narisanega znaka");
+
+                Point prva = points[0];
+                Point zadnja = points[points.Count - 1];
+
+                g.DrawRectangle(Pens.Red, prva.X - 1, prva.Y - 1, 3, 3);
+                g.DrawRectangle(Pens.Red, zadnja.X - 1, zadnja.Y - 1, 3, 3);
+
+                decimal N = numVectors.Value;
+
+                List<Point> tmp = new List<Point>(points);
+                Point zacasna = new Point();
+
+                do
+                {
+                    for (int i = 1; i < tmp.Count - 2; i++)
+                    {
+                        zacasna.X = (tmp[i].X + tmp[i + 1].X) / 2;
+                        zacasna.Y = (tmp[i].Y + tmp[i + 1].Y) / 2;
+
+                        tmp[i] = zacasna;
+                        tmp.RemoveAt(i+1);
+                        if (tmp.Count <= N + 1)
+                        {
+                            break;
+                        }
+                    }
+                } while (tmp.Count > N + 1);
+
+                panel1.Controls.Clear(); panel1.Refresh();
+
+                double maxX = points[0].X;
+                double maxY = points[0].Y;
+
+                for (int i = 0; i < tmp.Count - 1; i++)
+                {
+
+                    if (tmp[i + 1].X > maxX) maxX = tmp[i + 1].X;
+                    if (tmp[i + 1].Y > maxY) maxY = tmp[i + 1].Y;
+
+                    g.DrawRectangle(Pens.Red, tmp[i].X - 1, tmp[i].Y - 1, 3, 3);
+                    g.DrawRectangle(Pens.Red, tmp[i+1].X - 1, tmp[i+1].Y - 1, 3, 3);
+
+                    g.DrawLine(Pens.Black, tmp[i], tmp[i + 1]);
+                }
+
+                normalizirane.Clear();
+                //normalizacija
+                for (int i = 0; i < tmp.Count; i++)
+                {
+
+                    normalizirane.Add(new(tmp[i].X / maxX, tmp[i].Y / maxY));
+                }
+
+                vnosi.Add(new(znak, normalizirane));
+            }
+            else
+            {
+                clicked = false;
+
+                Point prva = points[0];
+                Point zadnja = points[points.Count - 1];
+
+                g.DrawRectangle(Pens.Red, prva.X - 1, prva.Y - 1, 3, 3);
+                g.DrawRectangle(Pens.Red, zadnja.X - 1, zadnja.Y - 1, 3, 3);
+
+                decimal N = numVectors.Value;
+
+                List<Point> tmp = new List<Point>(points);
+                Point zacasna = new Point();
+
+                do
+                {
+                    for (int i = 1; i < tmp.Count - 2; i++)
+                    {
+                        zacasna.X = (tmp[i].X + tmp[i + 1].X) / 2;
+                        zacasna.Y = (tmp[i].Y + tmp[i + 1].Y) / 2;
+
+                        tmp[i] = zacasna;
+                        tmp.RemoveAt(i+1);
+                        if (tmp.Count <= N + 1)
+                        {
+                            break;
+                        }
+                    }
+                } while (tmp.Count > N + 1);
+
+                panel1.Controls.Clear(); panel1.Refresh();
+
+                double maxX = points[0].X;
+                double maxY = points[0].Y;
+
+                for (int i = 0; i < tmp.Count - 1; i++)
+                {
+
+                    if (tmp[i + 1].X > maxX) maxX = tmp[i + 1].X;
+                    if (tmp[i + 1].Y > maxY) maxY = tmp[i + 1].Y;
+
+                    g.DrawRectangle(Pens.Red, tmp[i].X - 1, tmp[i].Y - 1, 3, 3);
+                    g.DrawRectangle(Pens.Red, tmp[i+1].X - 1, tmp[i+1].Y - 1, 3, 3);
+
+                    g.DrawLine(Pens.Black, tmp[i], tmp[i + 1]);
+                }
+
+                normalizirane.Clear();
+                //normalizacija
+                for (int i = 0; i < tmp.Count; i++)
+                {
+
+                    normalizirane.Add(new(tmp[i].X / maxX, tmp[i].Y / maxY));
+                }
+
+                vnosi.Add(new("", normalizirane));
+            }
+        }
+    }
+    public static class PupUpForm
+    {
+        public static string ShowDialog(string text, string caption = "")
+        {
+            Form popUpForm = new Form()
+            {
+                Width = 250,
+                Height = 100,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label textLabel = new Label() { Left = 10, Top = 10, Text = text };
+            TextBox textBox = new TextBox() { Left = 10, Top = 30, Width = 150 };
+            Button confirmation = new Button() { Text = "Ok", Left = 160, Width = 50, Top = 30, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { popUpForm.Close(); };
+            popUpForm.Controls.Add(textBox);
+            popUpForm.Controls.Add(confirmation);
+            popUpForm.Controls.Add(textLabel);
+            popUpForm.AcceptButton = confirmation;
+
+            return popUpForm.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+    }
+}
